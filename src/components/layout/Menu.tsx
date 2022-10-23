23b237b261bef7392989717aab6502e520702c6a
Ivan Veselinov Georgiev <it.work.bulgaria@gmail.com>
2022-10-23 16:28:51 +0300

    adding aditional user and user info

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -25,30 +25,32 @@ const Menu = () => {
         to="/daily-tasks"
         className={({ isActive }) =>
           isActive
-            ? "col-start-3 col-span-1  stroke-orange"
-            : "col-start-3 col-span-1 stroke-black"
+            ? "col-start-3 col-span-1  fill-orange"
+            : "col-start-3 col-span-1 fill-black"
         }
       >
         <svg
+          width="36px"
+          height="36px"
+          className="h-5 w-5 hover:fill-orange  fill-inherit"
+          viewBox="0 0 36 36"
+          version="1.1"
+          preserveAspectRatio="xMidYMid meet"
           xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 hover:stroke-orange  stroke-inherit"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
         >
+          <title>tasks-solid</title>
           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
-          />
+            className="clr-i-solid clr-i-solid-path-1"
+            d="M29.29,4.95h-7.2a4.31,4.31,0,0,0-8.17,0H7A1.75,1.75,0,0,0,5,6.64V32.26a1.7,1.7,0,0,0,1.71,1.69H29.29A1.7,1.7,0,0,0,31,32.26V6.64A1.7,1.7,0,0,0,29.29,4.95Zm-18,3a1,1,0,0,1,1-1h3.44V6.32a2.31,2.31,0,0,1,4.63,0V7h3.44a1,1,0,0,1,1,1V9.8H11.25Zm14.52,9.23-9.12,9.12-5.24-5.24a1.4,1.4,0,0,1,2-2l3.26,3.26,7.14-7.14a1.4,1.4,0,1,1,2,2Z"
+          ></path>
+          <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
         </svg>
       </NavLink>
       <NavLink
         to="/user"
         className={({ isActive }) =>
           isActive
-            ? "col-start-4 col-span-1 fill-orange "
+            ? "col-start-4 col-span-1 fill-orange"
             : "col-start-4 col-span-1 fill-black"
         }
       >
